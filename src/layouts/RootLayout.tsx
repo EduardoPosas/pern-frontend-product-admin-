09d753fa95cab638a1a9e05350e87e8a072023d8
Alexander Pozas <a.posas@outlook.com>
2024-09-21 19:33:11 -0600

    feat: Add router component and configuring layouts

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+
+import Header from "../components/Header"
+
+function RootLayout() {
+  return (
+    <>
+      <Header />
+      <main className="w-[95%] max-w-4xl mx-auto px-5 py-10 mt-10 shadow rounded">
+        <Outlet />
+      </main>
+    </>
+  )
+}
+
+export default RootLayout
